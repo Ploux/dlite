103ebe926599ec33e5dc1ba2ae808b15e41a7c17
Peter Loux <prl83@msstate.edu>
2022-12-09 15:33:33 -0600

    add initial code

---
+++ dlite.py
@@ -0,0 +1,23 @@
+# Compares the performance of three search algorithms on a variable-sized grid, with both stationary and moving obstacles
+# The three search algorithms are: breadth-first search, A*, and D* Lite
+
+# we run the algorithms as follows
+# search(grid, heuristic)
+# where heuristics = "bfs", "astar", "dlite"
+
+# a grid is an array, consisting of a two-dimensional array of cells with 1 = occupied, 0 = empty, the start cell, the goal cell, the cell to block, and the time to block it. 
+# For stationary grid problems where no cell is blocked, the block cell and time are both None
+
+import sys
+import math
+import random
+import time
+import heapq
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import colors, cm, patches, gridspec, rc, rcParams, ticker, transforms, patheffects, lines, collections, markers, text, image, patches
+
+
+
