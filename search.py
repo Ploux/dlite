30a83c426cbd90a0aaf281218478bde970d41a8a
Peter Loux <prl83@msstate.edu>
2022-12-09 16:29:13 -0600

    try new approach, add imports

---
+++ search.py
@@ -0,0 +1,8 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import random
+import heapq
+import math
+import sys
+from collections import defaultdict, deque, Counter
+from itertools import combinations
