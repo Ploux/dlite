4ce11b8f80b65cf01b472bf66f0bf5802a617214
Peter Loux <prl83@msstate.edu>
2022-12-10 13:25:44 -0600

    add fifo lifo and priority queue

--- search.py
+++ search.py
@@ -72,7 +72,34 @@ def path_states(node):
     if node in (cutoff, failure, None): 
         return []
     return path_states(node.parent) + [node.state]
+
+# Queues
+
+FIFOQueue = deque
+
+LIFOQueue = list
+
+class PriorityQueue:
+    """A queue in which the item with minimum f(item) is always popped first."""
+
+    def __init__(self, items=(), key=lambda x: x): 
+        self.key = key
+        self.items = [] # a heap of (score, item) pairs
+        for item in items:
+            self.add(item)
+         
+    def add(self, item):
+        """Add item to the queuez."""
+        pair = (self.key(item), item)
+        heapq.heappush(self.items, pair)
+
+    def pop(self):
+        """Pop and return the item with min f(item) value."""
+        return heapq.heappop(self.items)[1]
     
+    def top(self): return self.items[0][1]
+
+    def __len__(self): return len(self.items)    
 
 # report stats on search algorithms
 class CountCalls:
