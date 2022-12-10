f0f4da1fb47b9d7b336b91e9912fe8053cab48f6
Peter Loux <prl83@msstate.edu>
2022-12-10 12:11:45 -0600

    add metric reporting system for search algorithms

--- search.py
+++ search.py
@@ -17,6 +17,7 @@ from collections import defaultdict, deque, Counter
 from itertools import combinations
 
 
+# Problems and Nodes
 class Problem(object):
     """The abstract class for a formal problem. A new domain subclasses this,
     overriding `actions` and `results`, and perhaps other methods.
@@ -36,8 +37,6 @@ class Problem(object):
     def __str__(self):
         return '{}({!r}, {!r})'.format(
             type(self).__name__, self.initial, self.goal)
-    
-
 class Node:
     "A Node in a search tree."
     def __init__(self, state, parent=None, action=None, path_cost=0):
@@ -73,3 +72,36 @@ def path_states(node):
     if node in (cutoff, failure, None): 
         return []
     return path_states(node.parent) + [node.state]
+    
+
+# report stats on search algorithms
+class CountCalls:
+    """Delegate all attribute gets to the object, and count them in ._counts"""
+    def __init__(self, obj):
+        self._object = obj
+        self._counts = Counter()
+        
+    def __getattr__(self, attr):
+        "Delegate to the original object, after incrementing a counter."
+        self._counts[attr] += 1
+        return getattr(self._object, attr)
+
+        
+def report(searchers, problems, verbose=True):
+    """Show summary statistics for each searcher (and on each problem unless verbose is false)."""
+    for searcher in searchers:
+        print(searcher.__name__ + ':')
+        total_counts = Counter()
+        for p in problems:
+            prob   = CountCalls(p)
+            soln   = searcher(prob)
+            counts = prob._counts; 
+            counts.update(actions=len(soln), cost=soln.path_cost)
+            total_counts += counts
+            if verbose: report_counts(counts, str(p)[:40])
+        report_counts(total_counts, 'TOTAL\n')
+        
+def report_counts(counts, name):
+    """Print one line of the counts report."""
+    print('{:9,d} nodes |{:9,d} goal |{:5.0f} cost |{:8,d} actions | {}'.format(
+          counts['result'], counts['is_goal'], counts['cost'], counts['actions'], name))    
