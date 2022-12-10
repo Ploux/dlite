a3396307847f27eec7e1f8bef777cbbb2ac9b37b
Peter Loux <prl83@msstate.edu>
2022-12-10 15:59:01 -0600

    improve grid

--- search.py
+++ search.py
@@ -236,55 +236,262 @@ def transpose(matrix): return list(zip(*matrix))
 # puts a border around the grid
 # renders each cell as a square
 
+%matplotlib inline
+import matplotlib.pyplot as plt
+import random
+import heapq
+import math
+import sys
+from collections import defaultdict, deque, Counter
+from itertools import combinations
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+import random
+import heapq
+import math
+import sys
+from collections import defaultdict, deque, Counter
+from itertools import combinations
+
+
+# Problems and Nodes
+class Problem(object):
+    """The abstract class for a formal problem. A new domain subclasses this,
+    overriding `actions` and `results`, and perhaps other methods.
+    The default heuristic is 0 and the default action cost is 1 for all states.
+    When yiou create an instance of a subclass, specify `initial`, and `goal` states 
+    (or give an `is_goal` method) and perhaps other keyword args for the subclass."""
+
+    def __init__(self, initial=None, goal=None, **kwds): 
+        self.__dict__.update(initial=initial, goal=goal, **kwds) 
+        
+    def actions(self, state):        raise NotImplementedError
+    def result(self, state, action): raise NotImplementedError
+    def is_goal(self, state):        return state == self.goal
+    def action_cost(self, s, a, s1): return 1
+    def h(self, node):               return 0
+    
+    def __str__(self):
+        return '{}({!r}, {!r})'.format(
+            type(self).__name__, self.initial, self.goal)
+class Node:
+    "A Node in a search tree."
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.__dict__.update(state=state, parent=parent, action=action, path_cost=path_cost)
+
+    def __repr__(self): return '<{}>'.format(self.state)
+    def __len__(self): return 0 if self.parent is None else (1 + len(self.parent))
+    def __lt__(self, other): return self.path_cost < other.path_cost
+    
+    
+failure = Node('failure', path_cost=math.inf) # Indicates an algorithm couldn't find a solution.
+cutoff  = Node('cutoff',  path_cost=math.inf) # Indicates iterative deepening search was cut off.
+    
+    
+def expand(problem, node):
+    "Expand a node, generating the children nodes."
+    s = node.state
+    for action in problem.actions(s):
+        s1 = problem.result(s, action)
+        cost = node.path_cost + problem.action_cost(s, action, s1)
+        yield Node(s1, node, action, cost)
+        
+
+def path_actions(node):
+    "The sequence of actions to get to this node."
+    if node.parent is None:
+        return []  
+    return path_actions(node.parent) + [node.action]
+
+
+def path_states(node):
+    "The sequence of states to get to this node."
+    if node in (cutoff, failure, None): 
+        return []
+    return path_states(node.parent) + [node.state]
+
+# Queues
+
+FIFOQueue = deque
+
+LIFOQueue = list
+
+class PriorityQueue:
+    """A queue in which the item with minimum f(item) is always popped first."""
+
+    def __init__(self, items=(), key=lambda x: x): 
+        self.key = key
+        self.items = [] # a heap of (score, item) pairs
+        for item in items:
+            self.add(item)
+         
+    def add(self, item):
+        """Add item to the queuez."""
+        pair = (self.key(item), item)
+        heapq.heappush(self.items, pair)
+
+    def pop(self):
+        """Pop and return the item with min f(item) value."""
+        return heapq.heappop(self.items)[1]
+    
+    def top(self): return self.items[0][1]
+
+    def __len__(self): return len(self.items)    
+
+# straight-line distance between two points
+def straight_line_distance(A, B):
+    "Straight-line distance between two points."
+    return sum(abs(a - b)**2 for (a, b) in zip(A, B)) ** 0.5
+    
+# report stats on search algorithms
+class CountCalls:
+    """Delegate all attribute gets to the object, and count them in ._counts"""
+    def __init__(self, obj):
+        self._object = obj
+        self._counts = Counter()
+        
+    def __getattr__(self, attr):
+        "Delegate to the original object, after incrementing a counter."
+        self._counts[attr] += 1
+        return getattr(self._object, attr)
+
+        
+def report(searchers, problems, verbose=True):
+    """Show summary statistics for each searcher (and on each problem unless verbose is false)."""
+    for searcher in searchers:
+        print(searcher.__name__ + ':')
+        total_counts = Counter()
+        for p in problems:
+            prob   = CountCalls(p)
+            soln   = searcher(prob)
+            counts = prob._counts; 
+            counts.update(actions=len(soln), cost=soln.path_cost)
+            total_counts += counts
+            if verbose: report_counts(counts, str(p)[:40])
+        report_counts(total_counts, 'TOTAL\n')
+        
+def report_counts(counts, name):
+    """Print one line of the counts report."""
+    print('{:9,d} nodes |{:9,d} goal |{:5.0f} cost |{:8,d} actions | {}'.format(
+          counts['result'], counts['is_goal'], counts['cost'], counts['actions'], name))    
+
+# Grid Problems
+class GridProblem(Problem):
+    """Finding a path on a 2D grid with obstacles. Obstacles are (x, y) cells."""
+    # a grid has a height, width, initial, goal, and obstacles
+    def __init__(self, height=10, width=10, initial=(0, 0), goal=(9, 9), obstacles=(), **kwds):
+        Problem.__init__(self, height=height, width=width, initial=initial, goal=goal, obstacles=set(obstacles) - {initial, goal}, **kwds)
+
+    directions = [(-1, -1), (0, -1), (1, -1),
+                  (-1, 0),           (1,  0),
+                  (-1, +1), (0, +1), (1, +1)]
+    
+    def action_cost(self, s, action, s1): return straight_line_distance(s, s1)
+    
+    def h(self, node): return straight_line_distance(node.state, self.goal)
+                  
+    def result(self, state, action): 
+        "Both states and actions are represented by (x, y) pairs."
+        return action if action not in self.obstacles else state
+    
+    def actions(self, state):
+        """You can move one cell in any of `directions` to a non-obstacle cell."""
+        x, y = state
+        return {(x + dx, y + dy) for (dx, dy) in self.directions} - self.obstacles
+
+
+
+# Grid Creation
+
+## The following can be used to create obstacles:
+    
+def random_lines(X=range(15, 130), Y=range(60), N=150, lengths=range(6, 12)):
+    """The set of cells in N random lines of the given lengths."""
+    result = set()
+    for _ in range(N):
+        x, y = random.choice(X), random.choice(Y)
+        dx, dy = random.choice(((0, 1), (1, 0)))
+        result |= line(x, y, dx, dy, random.choice(lengths))
+    return result
+
+def line(x, y, dx, dy, length):
+    """A line of `length` cells starting at (x, y) and going in (dx, dy) direction."""
+    return {(x + i * dx, y + i * dy) for i in range(length)}
+
+random.seed(42) # To make this reproducible
+
+frame = line(-10, 20, 0, 1, 20) | line(150, 20, 0, 1, 20)
+cup = line(102, 44, -1, 0, 15) | line(102, 20, -1, 0, 20) | line(102, 44, 0, -1, 24)
+
+# Some grid routing problems
+
+'''
+# a small grid, only 5x4 cells
+# The goal is in the lower left corner (0, 0)
+# The start is on the right side, three cells up from the bottom (2, 3)
+# The 3 obstacles are at (1, 1), (2, 1), and (2, 0)
+'''
+
+small = GridProblem(height=4, width=5, initial=(2, 3), goal=(0, 0), 
+                    obstacles=[(1, 1), (2, 1), (2, 0)])
+                    
+    
+    
+# Algorithms
+          
+# Best First Search
+          
+def best_first_search(problem, f):
+    "Search nodes with minimum f(node) value first."
+    global reached
+    node = Node(problem.initial)
+    frontier = PriorityQueue([node], key=f)
+    reached = {problem.initial: node}
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        for child in expand(problem, node):
+            s = child.state
+            if s not in reached or child.path_cost < reached[s].path_cost:
+                reached[s] = child
+                frontier.add(child)
+    return failure
+
+# Breadth First Search
+
+def breadth_first_bfs(problem):
+    "Search shallowest nodes in the search tree first; using best-first."
+    return best_first_search(problem, f=len)    
+
+# Plotting
+
+    
+def transpose(matrix): return list(zip(*matrix))
+
+# an improved grid plotting function
+# puts a border around the grid
+# renders each cell as a square
+
 def plot_grid_problem(grid, solution, reached=(), title='Search', show=True):
-    "Use pyplot to plot the grid, the solution, and the reached cells."
-    # create a grid of cells
-    # each cell is a square with a border
-    # the cell is colored according to the state of the cell
-    # white: unexplored
-    # gray: explored but not part of the solution
-    # black: part of the solution
-    # the goal is a red square
-    # the start is a green square
-    # the obstacles are blue squares
-    
-    plt.figure(figsize=(10, 10))
-    plt.title(title)
-    plt.axis('off')
-    plt.xlim(-1, grid.width)
-    plt.ylim(-1, grid.height)
-    plt.gca().invert_yaxis()
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.xticks(range(grid.width))
-    plt.yticks(range(grid.height))
-    plt.grid(True)
-    
-    # draw the grid
-    for x in range(grid.width):
-        for y in range(grid.height):
-            color = 'white'
-            if (x, y) in reached:
-                color = 'gray'
-            if (x, y) in solution:
-                color = 'black'
-            if (x, y) == grid.goal:
-                color = 'red'
-            if (x, y) == grid.initial:
-                color = 'green'
-            if (x, y) in grid.obstacles:
-                color = 'blue'
-            plt.gca().add_patch(plt.Rectangle((x, y), 1, 1, color=color, ec='black'))
-            
-    # draw the solution
-    if solution:
-        plt.plot([x for (x, y) in solution], [y for (x, y) in solution], 'k-')
+    "Use matplotlib to plot the grid, obstacles, solution, and reached."
+    reached = list(reached)
+    # make the grid width x height
+    plt.figure(figsize=(grid.width, grid.height))
+    # and draw a border around it
+    plt.plot([0, grid.width, grid.width, 0, 0], [0, 0, grid.height, grid.height, 0], 'k-')
+    plt.axis('off'); plt.axis('equal')
+    plt.scatter(*transpose(grid.obstacles), marker='s', color='darkgrey')
+    plt.scatter(*transpose(reached), 1**2, marker='.', c='blue')
+    plt.scatter(*transpose(path_states(solution)), marker='s', c='blue')
+    plt.scatter(*transpose([grid.initial]), 9**2, marker='D', c='green')
+    plt.scatter(*transpose([grid.goal]), 9**2, marker='8', c='red')
+    if show: plt.show()
+    print('{} {} search: {:.1f} path cost, {:,d} states reached'
+          .format(' ' * 10, title, solution.path_cost, len(reached)))
         
-    if show:
-        plt.show()
-        
-        
-                
-    
+ 
         
 # Testing
 
